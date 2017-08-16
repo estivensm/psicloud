d9e51ce4485442cebcc7dbb676c164eda05161dc
alejovelez10 <alejovelez10@gmail.com>
2017-08-15 21:34:03 -0500

    Configurando Devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require jquery
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
+
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    [:account_update,:sign_up].each do |metodo|
+    devise_parameter_sanitizer.permit(metodo, keys: [:email,:names,:last_names,:admin_user,:rol_id,:avatar, :birth_date,:document,:document_type, :medical_record,:state])
+    end
+  end
+
+
+
+layout :layout_for_selection
+protected
+  def layout_for_selection
+    if controller_name == 'sessions'  || controller_name == 'passwords' 
+      'application'
+    elsif controller_name == 'registrations'
+
+        if action_name == "new" || action_name == "create"
+            'application'
+            else
+            	'admin'
+        end
+    	else
+      'admin'
+    end
+  end
 end
