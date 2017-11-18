68e26a36988bf46a0329e9a5e7ef897d01b630ed
alejovelez10 <alejovelez10@gmail.com>
2017-11-18 18:30:55 -0500

    fmail

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -12,7 +12,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
    #storage :file
   # storage :fog
-
+storage (Rails.env.production? ? :fog : :file)
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
