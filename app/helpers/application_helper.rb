b187725040671f776d005f32de9047be265dc0b1
alejovelez10 <alejovelez10@gmail.com>
2018-02-26 21:57:34 -0500

    organizando front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -128,7 +128,7 @@ def menu
         @menu = ["active1","na","na","na","na","na", "na","na"]
      elsif controller.controller_name == "views"
         @menu = ["na","na","na","na","active1","na", "na","na"]
-     elsif controller.controller_name == "patients" && (action_name == "index" || action_name == "new" || action_name == "show")
+     elsif (controller.controller_name == "patients" && (action_name == "index" || action_name == "new" || action_name == "show")) || controller_name == "child_general_dates" || controller_name == "personal_histories" || controller_name == "two_child_histories" || controller_name == "three_child_histories" || controller_name == "tracings" || controller_name == "tool_tests"
         @menu = ["na","na","active1","na","na","na", "na","na"]
      elsif controller.controller_name == "appointments" && action_name == "citas"
         @menu = ["na","na","na","active1","na","na", "na","na"]
