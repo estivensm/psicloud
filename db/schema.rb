c88d27c0ff5032ce4275bac349ba4aaf461ed2cb
alejovelez10 <alejovelez10@gmail.com>
2018-03-03 15:56:13 -0500

    validate form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209203455) do
+ActiveRecord::Schema.define(version: 20180302234227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -441,6 +441,7 @@ ActiveRecord::Schema.define(version: 20180209203455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "company"
+    t.boolean "legal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
