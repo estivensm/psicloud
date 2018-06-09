b50f4696199301a6ca089b270e1896666c391d92
alejovelez10 <alejovelez10@gmail.com>
2018-06-08 22:29:05 -0500

    organizando paciente

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,7 +101,7 @@ class User < ApplicationRecord
 end
 
 def token_expired?
-  expiry = Time.at(self.expires_ats) 
+  expiry = Time.at(self.expires_ats.to_l) 
    puts "entre aquiiiiiiiiiiiiiiiiiiiiiiiiiuuuuuuuuuuuuuuuuuuuuuuuuuuuuuiii"
   return true if expiry < Time.now # expired token, so we should quickly return
   token_expires_at = expiry
