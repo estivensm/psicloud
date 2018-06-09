51d52bc8d7292d56aebc21d2af3aa6b2c1861777
alejovelez10 <alejovelez10@gmail.com>
2018-06-08 20:18:54 -0500

    exiration google

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -271,8 +271,8 @@ if fecha != nil
     dm = fecha.strftime("%d") #Dia del mes
     m = fecha.strftime("%m") # Mes del Año
     meses = {"01" => "Enero", "02" => "Febrero","03"=>"Marzo","04" => "Abril", "05" => "Mayo","06"=> "Junio" ,"07"=> "Julio", "08" => "Agosto", "09"=> "Septiembre" ,"10"=> "Octubre","11" => "Noviembre" ,"12" => "Diciembre" }
-    dias = {"7" => "Domingo", "1" => "Lunes","2"=>"Martes","3" => "Miercoles", "4" => "Jueves","5"=> "Viernes" ,"6" =>"Sabado"}
-    return  meses[m] + " " + dm + ", " + y  
+    dias = {"0" => "Domingo", "1" => "Lunes","2"=>"Martes","3" => "Miercoles", "4" => "Jueves","5"=> "Viernes" ,"6" =>"Sabado"}
+    return  dias[ds] + " " + dm + " de " + meses[m] + ", " + y  
 #dias[ds] + ", " +
 end 
 end
@@ -284,13 +284,13 @@ if fecha != nil
     y = fecha.strftime("%Y") #Año
     dm = fecha.strftime("%d") #Dia del mes
     m = fecha.strftime("%m") # Mes del Año
-    meses = {"01" => "Enero", "02" => "Febrero","03"=>"Marzo","04" => "Abril", "05" => "Mayo","06"=> "Junio" ,"07"=> "Julio", "08" => "Agosto", "09"=> "Septiembre" ,"10"=> "Octubre","11" => "Noviembre" ,"12" => "Diciembre" }
-    dias = {"7" => "Domingo", "1" => "Lunes","2"=>"Martes","3" => "Miercoles", "4" => "Jueves","5"=> "Viernes" ,"6" =>"Sabado"}
+    meses = {"01" => "Ene", "02" => "Feb","03"=>"Mar","04" => "Abr", "05" => "May","06"=> "Jun" ,"07"=> "Jul", "08" => "Ago", "09"=> "Sep" ,"10"=> "Oct","11" => "Nov" ,"12" => "Dic" }
+    dias = {"0" => "Domingo", "1" => "Lunes","2"=>"Martes","3" => "Miercoles", "4" => "Jueves","5"=> "Viernes" ,"6" =>"Sabado"}
     fecha.min < 10 ? min = "0" : min = ""
     horat = fecha.hour > 12 ?   fecha.hour - 12 : fecha.hour
     horati = fecha.hour > 12 ?   "pm" : "am"
     fecha.hour < 10 ? hora = "0" : hora = ""
-    return  meses[m] + " " + dm + ", " + y + ", " + hora + horat.to_s   + ":" + min + fecha.min.to_s + " " + horati
+    return  dias[ds] + " " + dm + " de "+  meses[m] + " "  + ", " + y + ", " + hora + horat.to_s   + ":" + min + fecha.min.to_s + " " + horati
 #dias[ds] + ", " +
 end 
 end
