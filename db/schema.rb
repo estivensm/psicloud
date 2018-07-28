fa342ce48d4a9c21f2db4ace884cf622ffdc4882
alejovelez10 <alejovelez10@gmail.com>
2018-07-28 17:38:41 -0500

    cambio campos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180728201247) do
+ActiveRecord::Schema.define(version: 20180728201959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -247,6 +247,15 @@ ActiveRecord::Schema.define(version: 20180728201247) do
     t.integer "admin_user"
     t.boolean "requierd"
     t.string "form"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fieldselects", force: :cascade do |t|
+    t.integer "field_id"
+    t.integer "admin_user"
+    t.integer "user_id"
+    t.string "option"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
