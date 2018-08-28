2881cd17e02bec19b32cd88910a8ba25e532bee3
alejovelez10 <alejovelez10@gmail.com>
2018-08-27 19:42:04 -0500

    cambios

--- app/controllers/clinic_histories_controller.rb
+++ app/controllers/clinic_histories_controller.rb
@@ -157,11 +157,11 @@ end
 
           if !@clinic_history.child_history
           
-          redirect_to  patient_clinic_history_backgrounds_path(@clinic_history.patient_id,@clinic_history.id)  , notice: 'Clinic history was successfully created.' 
+          redirect_to  patient_clinic_history_history_tool_tests_path(@clinic_history.patient_id,@clinic_history.id)  , notice: 'Clinic history was successfully created.' 
            
             else
           
-          redirect_to  patient_clinic_history_backgrounds_path(@clinic_history.patient_id,@clinic_history.id)  , notice: 'Clinic history was successfully created.' 
+          redirect_to  patient_clinic_history_history_tool_tests_path(@clinic_history.patient_id,@clinic_history.id)  , notice: 'Clinic history was successfully created.' 
 
           
             end
@@ -199,7 +199,7 @@ end
         format.html { 
                if !@clinic_history.child_history && !@clinic_history.child_general_date
           
-          redirect_to  patient_clinic_history_backgrounds_path(@clinic_history.patient_id,@clinic_history.id), notice: 'Clinic history was successfully created.' 
+          redirect_to  patient_clinic_history_history_tool_tests_path(@clinic_history.patient_id,@clinic_history.id), notice: 'Clinic history was successfully created.' 
            
             elsif !@clinic_history.child_history && @clinic_history.child_general_date
           
