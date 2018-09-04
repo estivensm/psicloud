6d901fd035f7055572f41577401d76a13a82f933
alejovelez10 <alejovelez10@gmail.com>
2018-09-04 11:25:35 -0500

    organizando tareas

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -4,7 +4,7 @@ class FieldsController < ApplicationController
   # GET /fields
   # GET /fields.json
   def index
-    @fields = Field.all
+    @fields = Field.where(admin_user: current_user.admin_user)
   end
 
   # GET /fields/1
